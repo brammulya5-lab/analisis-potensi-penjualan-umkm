92f2621ae81f3834e7b30a3e67563b9863fc7fa1
brammulya5-lab <brammulya5@gmail.com>
2026-07-29 12:07:10 +0700

    Perbaikan code

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
     page_title="Analisis Potensi Penjualan UMKM",
     page_icon="📈",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="auto"
 )
 
 # DESIGN TOKENS (TEMA TERANG PROFESIONAL)
